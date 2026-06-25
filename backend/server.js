7a1691fc25fdd47aff670c8580242584f4c6f320
POLANA KAMAL KALYAN <polanakamalkalyan3@gmail.com>
2026-06-25 23:33:15 +0530

    configure backend for vercel

--- backend/server.js
+++ backend/server.js
@@ -24,8 +24,4 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-});
+module.exports = app;
