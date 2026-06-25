e48e7613693708152a7f7da79824fa3cc44752f1
POLANA KAMAL KALYAN <polanakamalkalyan3@gmail.com>
2026-06-26 00:20:25 +0530

    fix vercel backend routing

---
+++ backend/api/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+// Load env vars
+dotenv.config();
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/transactions', require('./routes/transactions'));
+app.use('/api/categories', require('./routes/categories'));
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+module.exports = app;
