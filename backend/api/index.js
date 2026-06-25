920260b24c783a0264529a6dafc043b3ffdbae04
POLANA KAMAL KALYAN <polanakamalkalyan3@gmail.com>
2026-06-26 00:38:50 +0530

    fix api index

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,23 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const connectDB = require('../config/db');
 
-// Load env vars
 dotenv.config();
 
-// Connect to database
 connectDB();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
-const connectDB = require('../config/db');
 
 app.use('/api/auth', require('../routes/auth'));
 app.use('/api/transactions', require('../routes/transactions'));
 app.use('/api/categories', require('../routes/categories'));
+
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
