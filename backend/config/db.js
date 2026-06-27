308b246441138d908212224772f4e585a6da6a93
POLANA KAMAL KALYAN <polanakamalkalyan3@gmail.com>
2026-06-27 09:37:46 +0530

    prepare backend for Render

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URL);
-
+        const conn = await mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://<polanakamalkalyan3_db_user>:ZtQrZ2YMeaOw8xo2@personal-finance-tracke.i1dribs.mongodb.net/?appName=Personal-Finance-Tracker');
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
--- backend/server.js
+++ backend/server.js
@@ -24,4 +24,8 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
