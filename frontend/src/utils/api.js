a379dcfca837ef4f4adc2bb85dd62620bde2bd41
POLANA KAMAL KALYAN <polanakamalkalyan3@gmail.com>
2026-06-27 22:12:37 +0530

    Fix Axios baseURL in api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,9 +1,10 @@
+
 import axios from 'axios';
 
 const api = axios.create({
-    //baseURL: 'http://localhost:5000/api',
+     //baseURL: 'http://localhost:5000/api',
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
-     baseURL: import.meta.env.VITE_API_URL, 
         'Content-Type': 'application/json'
     }
 });
@@ -17,9 +18,7 @@ api.interceptors.request.use(
         }
         return config;
     },
-    (error) => {
-        return Promise.reject(error);
-    }
+    (error) => Promise.reject(error)
 );
 
 export default api;
