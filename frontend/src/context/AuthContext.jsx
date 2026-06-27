037c5f6c123d21768b4fddd8976997caf732fdbd
POLANA KAMAL KALYAN <polanakamalkalyan3@gmail.com>
2026-06-27 22:40:34 +0530

    edited api feach

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -26,13 +26,13 @@ export const AuthProvider = ({ children }) => {
     }, []);
 
     const login = async (email, password) => {
-        const res = await api.post('/auth/login', { email, password });
+        const res = await api.post('api/auth/login', { email, password });
         localStorage.setItem('token', res.data.token);
         setUser(res.data.user);
     };
 
     const register = async (name, email, password) => {
-        const res = await api.post('/auth/register', { name, email, password });
+        const res = await api.post('/api/auth/register', { name, email, password });
         localStorage.setItem('token', res.data.token);
         setUser(res.data.user);
     };
